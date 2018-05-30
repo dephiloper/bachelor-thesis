e1c9b4865f494967ae7fcdd316a7ce85ede10bac
dephiloper <s0554213@htw-berlin.de>
2018-05-30 17:16:45 +0200

    trying to get the physics system working

--- BachelorThesis/Assets/Scripts/BallResetScript.cs
+++ BachelorThesis/Assets/Scripts/BallResetScript.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Linq.Expressions;
+using UnityEngine;
 
 public class BallResetScript : MonoBehaviour
 {
     public GameObject Ball;
-    
-    private SteamVR_Controller.Device TrackedDevice => SteamVR_Controller.Input((int) _trackedObj.index);
     private SteamVR_TrackedController _controller;
+    
+    /*private SteamVR_Controller.Device TrackedDevice => SteamVR_Controller.Input((int) _trackedObj.index);
     private SteamVR_TrackedObject _trackedObj;
 
     private void Awake()
@@ -41,5 +42,30 @@ public class BallResetScript : MonoBehaviour
         Ball.GetComponent<Rigidbody>().isKinematic = false;
         ballRigidBody.velocity = TrackedDevice.velocity;
         ballRigidBody.angularVelocity = TrackedDevice.angularVelocity;
+    }*/
+    
+    private void OnEnable()
+    {
+        _controller = GetComponent<SteamVR_TrackedController>();
+        InvokeRepeating(nameof(RelocateBall), 0f, 4.0f); 
+        InvokeRepeating(nameof(LaunchBall), 2.0f, 4.0f);
+    }
+    
+    private void RelocateBall()
+    {
+        Ball.transform.position = new Vector3(0, 1.1f, 1);
+        var ballRb = Ball.GetComponent<Rigidbody>();
+        ballRb.angularVelocity = Vector3.zero;
+        ballRb.transform.rotation = Quaternion.identity;
+        ballRb.useGravity = false;
+        ballRb.isKinematic = true;
+    }
+
+    private void LaunchBall()
+    {
+        var ballRb = Ball.GetComponent<Rigidbody>();
+        ballRb.useGravity = true;
+        ballRb.isKinematic = false;
+        ballRb.AddForce(Vector3.back * 1.5f);
     }
 }
