2c2e27b5528a884cc9d2d94e6bf0d98f54a2b96d
dephiloper <philippboensch@gmail.com>
2018-06-04 13:01:57 +0200

    improvements on the environment

--- BachelorThesis/Assets/Scripts/BallResetScript.cs
+++ BachelorThesis/Assets/Scripts/BallResetScript.cs
@@ -3,31 +3,48 @@ using UnityEngine;
 
 public class BallResetScript : MonoBehaviour
 {
+    /// <summary>
+    /// speed that is applied to the ball when the launch ball method gets invoked
+    /// </summary>
     public float Speed = 1f;
-    private Rigidbody _ballRb;
-    private Vector3 _startPos;
+    
+    /// <summary>
+    /// the rigid body of the table tennis ball
+    /// </summary>
+    private Rigidbody _ballRigidbody;
+    
+    /// <summary>
+    /// the start position in the global space
+    /// </summary>
+    private Vector3 _startPosition;
     
     private void OnEnable()
     {
-        _ballRb = GetComponent<Rigidbody>();
-        _startPos = transform.position;
-        InvokeRepeating(nameof(RelocateBall), 0f, 4.0f); 
+        _ballRigidbody = GetComponent<Rigidbody>();
+        _startPosition = transform.position;
+        InvokeRepeating(nameof(ResetBallPosition), 0f, 4.0f); 
         InvokeRepeating(nameof(LaunchBall), 2.0f, 4.0f);
     }
-    
-    private void RelocateBall()
+
+    /// <summary>
+    /// moves the ball position to its initial value with initial rotation directions
+    /// </summary>
+    private void ResetBallPosition()
     {
-        transform.position = _startPos;
-        _ballRb.angularVelocity = Vector3.zero;
-        _ballRb.transform.rotation = Quaternion.identity;
-        _ballRb.useGravity = false;
-        _ballRb.isKinematic = true;
+        transform.position = _startPosition;
+        _ballRigidbody.angularVelocity = Vector3.zero;
+        _ballRigidbody.transform.rotation = Quaternion.identity;
+        _ballRigidbody.useGravity = false;
+        _ballRigidbody.isKinematic = true;
     }
 
+    /// <summary>
+    /// applies a force to the ball and enables gravity
+    /// </summary>
     private void LaunchBall()
     {
-        _ballRb.useGravity = true;
-        _ballRb.isKinematic = false;
-        _ballRb.AddForce(Vector3.forward * Speed);
+        _ballRigidbody.useGravity = true;
+        _ballRigidbody.isKinematic = false;
+        _ballRigidbody.AddForce(Vector3.forward * Speed);
     }
 }
