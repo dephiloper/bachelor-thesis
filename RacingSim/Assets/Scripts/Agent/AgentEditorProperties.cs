134eb12d3edd84596d1c33e259fbdfa02e525c98
Philipp <philippboensch@gmail.com>
2018-06-28 22:37:01 +0200

    added agent properties

---
+++ RacingSim/Assets/Scripts/Agent/AgentEditorProperties.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Agent
+{
+    [Serializable]
+    public abstract class BaseEditorProperties
+    {
+        public float MaxSpeed = 10f;
+        public float TurnSpeed = 2f;
+        public int Score;
+        public float Speed;
+    }
+
+    [Serializable]
+    public class PlayerEditorProperties : BaseEditorProperties
+    {
+        public string HAxis;
+        public string VAxis;
+        public int SelectedHAxis;
+        public int SelectedVAxis;
+    }
+
+    [Serializable]
+    public class NeuralNetEditorProperties : BaseEditorProperties
+    {
+        public TextAsset BrainAsset;
+        public List<int> ReachedWaypointIds = new List<int>();
+        public bool IsTrained;
+        public bool IsExclude;
+    }
+}
