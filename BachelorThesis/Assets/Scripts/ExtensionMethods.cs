ae02f20ccce935a319d2cfc1e5a8f7a509cd5e57
Philipp <philippboensch@gmail.com>
2018-07-13 14:47:44 +0200

    fixed wrong newtonsoft version bug

--- BachelorThesis/Assets/Scripts/ExtensionMethods.cs
+++ BachelorThesis/Assets/Scripts/ExtensionMethods.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NNSharp.DataTypes;
+using NNSharp.Models;
+using UnityEngine;
 
 public static class ExtensionMethods
 {
@@ -7,4 +12,50 @@ public static class ExtensionMethods
 
     public static float Map(this float value, float inFrom, float inTo, float outFrom, float outTo) =>
         (value - inFrom) / (inTo - inFrom) * (outTo - outFrom) + outFrom;
+
+    public static void SetWeights(this SequentialModel sequentialModel, int[] layers, double[] weights)
+    {
+        if (sequentialModel.GetInputDimension().c != layers[0])
+            throw new ArgumentOutOfRangeException(nameof(ArgumentOutOfRangeException), "Size of input layer should be" +
+                                                       $"{sequentialModel.GetInputDimension().c} but is {layers[0]}.");     
+        
+        var data = new List<IData>();
+        var count = 0;
+
+        for (var i = 0; i < layers.Length - 1; i++)
+        {
+            // create weight list
+            data.Add(new Data2D(1, 1, layers[i], layers[i + 1]));
+            data.Add(new DataArray(layers[i + 1]));
+            data.Add(new DataArray(0));
+
+            // assign weights to weight list
+            for (var j = 0; j < layers[i]; j++)
+                for (var k = 0; k < layers[i + 1]; k++)
+                    ((Data2D) data[i * 3])[0, 0, j, k] = weights[count++];
+
+            // assign biases to weight list
+            for (var j = 0; j < layers[i + 1]; j++)
+                ((DataArray) data[i * 3 + 1])[j] = weights[count++];
+        }
+
+        sequentialModel.SetWeights(data);
+    }
+
+    public static int GetWeightCount(this SequentialModel sequentialModel, int[] layers)
+    {
+        return layers.Select((t, i) => t * layers[i + 1] + layers[i + 1]).Sum();
+    }
+
+    public static double[] ToDoubleArray(this Data2D data2D, int[] layers)
+    {
+        var outputLayer = layers[layers.Length - 1];
+        var arr = new double[outputLayer];
+        for (var i = 0; i < outputLayer; i++)
+        {
+            arr[i] = data2D[0, 0, i, 0];
+        }
+
+        return arr;
+    }
 }
