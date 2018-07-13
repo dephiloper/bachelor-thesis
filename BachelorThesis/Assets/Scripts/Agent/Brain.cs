26fefa4e00898004c2e2515e55058fd0d9100dcd
Philipp <philippboensch@gmail.com>
2018-07-13 13:47:08 +0200

    unstable commit / nnsharp is in a broken state

--- BachelorThesis/Assets/Scripts/Agent/Brain.cs
+++ BachelorThesis/Assets/Scripts/Agent/Brain.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Agent.Data;
 using Encog.Engine.Network.Activation;
 using Encog.ML.Data.Basic;
 using Encog.Neural.Networks;
 using Encog.Neural.Networks.Layers;
+using NNSharp.DataTypes;
+using NNSharp.IO;
 using Train;
 using UnityEngine;
 using Action = Agent.Data.Action;
 using Random = UnityEngine.Random;
+using NNSharp.Models;
 
 namespace Agent
 {
@@ -17,41 +21,34 @@ namespace Agent
         public double Fitness { get; set; }
         public float Score { get; set; }
         
-        private static readonly int[] DefaultLayer = {24, 13, 2};
-        private BasicNetwork Network { get; set; }
-
-        public Brain(params int[] layer)
+        private readonly int[] _layers = {24, 13, 2};
+        private readonly SequentialModel _model;
+        private double[] _weights;
+        
+        public Brain(string defaultModel = "plain_model.json")
         {
-            if (layer.Length == 0)
-                layer = DefaultLayer;
-            Setup(layer);
+            var reader = new ReaderKerasModel(defaultModel);
+            _model = reader.GetSequentialExecutor();
+            Setup();
         }
 
-        private Brain(double[] weights, params int[] layer) : this(layer)
+        private Brain(int[] layers, double[] weights)
         {
-            Network.Flat.Weights = weights;
+            _layers = layers;
+            _weights = weights;
+            _model.SetWeights(_layers, _weights);
         }
-
-        private void Setup(params int[] layer)
+        
+        private void Setup()
         {
-            Network = new BasicNetwork();
-
-            // Input Layer
-            Network.AddLayer(new BasicLayer(null, true, layer[0]));
-
-            // Hidden Layer
-            for (var i = 1; i < layer.Length - 1; i++)
-                Network.AddLayer(new BasicLayer(new ActivationReLU(), true, layer[i]));
-
-            // Output Layer
-            Network.AddLayer(new BasicLayer(new ActivationTANH(), false, layer[layer.Length - 1]));
-            Network.Structure.FinalizeStructure();
-            Network.Reset();
-
-#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-            for (var i = 0; i < Network.Flat.Weights.Length; i++)
-                Network.Flat.Weights[i] = Random.Range(-1f, 1f);
-#endif
+            var weightCount = _model.GetWeightCount(_layers);
+            _weights = new double[weightCount];
+            
+            // randomize initial weights
+            for (var i = 0; i < weightCount; i++)
+                _weights[i] = Random.Range(-1, 1);
+            
+            _model.SetWeights(_layers, _weights);
         }
 
         public Action Think(Percept percept)
@@ -59,71 +56,66 @@ namespace Agent
             var data = percept.ToDoubleArray();
 
 
-            if (data.Length != DefaultLayer[0])
+            if (data.Length != _model.GetInputDimension().c)
                 throw new ArgumentOutOfRangeException(nameof(ArgumentOutOfRangeException),
-                    $"Percept count should be {DefaultLayer[0]}, but is {data.Length}");
+                    $"Percept count should be {_model.GetInputDimension().c}, but is {data.Length}, check if the " +
+                    $"input layer in DefaultLayer is set to the right value (currently: {_layers[0]}");
             
-            var mlData = new BasicMLData(data);
-            var result = Network.Compute(mlData);
-            return new Action((result as BasicMLData)?.Data);
+            var input = new Data2D(1, 1, _layers[0], 1);
+            var result = (Data2D)_model.ExecuteNetwork(input);
+            return new Action(result.ToDoubleArray(_layers));
         }
 
         public void Mutate(float mutationRate)
         {
-            for (var i = 0; i < Network.Flat.Weights.Length; i++)
+            for (var i = 0; i < _weights.Length; i++)
                 if (Random.value < mutationRate)
-                    Network.Flat.Weights[i] = Mathf.PerlinNoise((float) Network.Flat.Weights[i], 0.0f);
+                    _weights[i] = Mathf.PerlinNoise((float) _weights[i], 0.0f);
         }
 
         public Brain[] OnePointCrossover(Brain partner)
         {
-            var weights = Network.Flat.Weights;
-            var partnerWeights = partner.Network.Flat.Weights;
-
-            var midpoint = Random.value * weights.Length;
+            var midpoint = Random.value * _weights.Length;
             // creates two children with the DNA of each of the parents
-            var childOne = (double[]) weights.Clone();
-            var childTwo = (double[]) weights.Clone();
+            var childOne = (double[]) _weights.Clone();
+            var childTwo = (double[]) _weights.Clone();
             if (Random.value < TrainManager.Instance.CrossoverProbabilty)
             {
-                for (var i = 0; i < weights.Length; i++)
+                for (var i = 0; i < _weights.Length; i++)
                 {
                     if (i < midpoint)
-                        childOne[i] = partnerWeights[i];
+                        childOne[i] = partner._weights[i];
                     else
-                        childTwo[i] = partnerWeights[i];
+                        childTwo[i] = partner._weights[i];
                 }
             }
 
-            return new[] {new Brain(childOne), new Brain(childTwo)};
+            return new[] {new Brain(_layers, childOne), new Brain(_layers, childTwo)};
         }
 
         public Brain[] UniformCrossover(Brain partner)
         {
-            var weights = Network.Flat.Weights;
-            var partnerWeights = partner.Network.Flat.Weights;
-
             // creates two children with the DNA of each of the parents
-            var childOne = (double[]) weights.Clone();
-            var childTwo = (double[]) weights.Clone();
+            var childOne = (double[]) _weights.Clone();
+            var childTwo = (double[]) _weights.Clone();
             if (Random.value < TrainManager.Instance.CrossoverProbabilty)
             {
-                for (var i = 0; i < weights.Length; i++)
+                for (var i = 0; i < _weights.Length; i++)
                 {
                     if (Random.value < TrainManager.Instance.UniformCrossoverProbability)
-                        childOne[i] = partnerWeights[i];
+                        childOne[i] = partner._weights[i];
                     else
-                        childTwo[i] = partnerWeights[i];
+                        childTwo[i] = partner._weights[i];
                 }
             }
 
-            return new[] {new Brain(childOne), new Brain(childTwo)};
+            return new[] {new Brain(_layers, childOne), new Brain(_layers, childTwo)};
         }
 
         public void Export(int generation, float maxLifespan, string path = "./Assets/Exports/")
         {
             Directory.CreateDirectory(path);
-            var serializableBrain = new SerializableBrain(this, generation, (int) maxLifespan);
+            var serializableBrain = new SerializableBrain(_layers, _weights);
             var jsonBrain = JsonUtility.ToJson(serializableBrain);
             path = $"{path}{DateTime.Now:yyyy-MM-dd-HH_mm-ss-ffffff}-gen_{generation}-score_{Score:F}-brain.json";
             File.WriteAllText(path, jsonBrain);
@@ -140,24 +132,20 @@ namespace Agent
 
         private class SerializableBrain
         {
-            [SerializeField] private int _score;
-            [SerializeField] private double[] _weights;
-            [SerializeField] private int _maxLifespan;
-            [SerializeField] private int _generation;
-
-            public SerializableBrain(Brain brain, int generation, int maxLifespan)
+            [SerializeField] 
+            private int[] _layers;
+            [SerializeField] 
+            private double[] _weights;
+            
+            public SerializableBrain(int[] layers, double[] weights)
             {
-                _score = (int) brain.Score;
-                _weights = brain.Network.Flat.Weights;
-                _generation = generation;
-                _maxLifespan = maxLifespan;
+                _layers = layers;
+                _weights = weights;
             }
 
             public Brain ToBrain()
             {
-                var brain = new Brain(_weights);
-                brain.Score = _score;
-
+                var brain = new Brain(_layers, _weights);
                 return brain;
             }
         }
