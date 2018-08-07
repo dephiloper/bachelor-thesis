d0155b30e6be7b40eeb7bb0413e7320dc2eab7b2
dephiloper <philippboensch@gmail.com>
2018-08-07 11:30:42 +0200

    training scene adapted to large vehicles

--- BachelorThesis/Assets/Scripts/AgentData/Actions/NeuralNetAction.cs
+++ BachelorThesis/Assets/Scripts/AgentData/Actions/NeuralNetAction.cs
@@ -13,21 +13,18 @@ namespace AgentData.Actions
         public bool SteerRight { get; }
         public float SteerValue { get; }
         public double[] Raw { get; }
-        public double ActivationThreshold { get; } = 0.3d;
+        public double ActivationThreshold { get; } = 0;
     
         public NeuralNetAction(double[] data)
         {
             //Debug.Log($"Input(vAxis:{data[0]}, hAxis: {data[1]})");
             Raw = data;
             
-            data[0] = Math.Round(data[0]);
-            data[1] = Math.Round(data[1]);
-                        
             AccelerateForward = data[0] > ActivationThreshold;
-            AccelerateBackward = data[0] < -ActivationThreshold;
+            AccelerateBackward = data[0] < ActivationThreshold;
             AccelerateValue = Mathf.Abs((float)data[0]);
             SteerLeft = data[1] < ActivationThreshold;
-            SteerRight = data[1] > -ActivationThreshold;
+            SteerRight = data[1] > ActivationThreshold;
             SteerValue = Mathf.Abs((float)data[1]);
         }
     }
